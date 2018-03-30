66d7bce2bfb7c91969703f5928be97cac9274ed6
Klara Boyer <kb@klaras-mbp-2.attlocal.net>
2018-03-29 23:27:23 -0400

    functioning app with minor bugs

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,7 +15,10 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
   if (err) throw err;
   displayItems();
-  console.log("CONSOLE: CONNECTED!");
+  console.log("Connected as id: " + connection.threadId + "\n");
+  console.log("");
+  console.log("======================================== THE BAMAZON PRODUCT SELECTION ========================================");
+  console.log("");
 });
 
 function displayItems() {
@@ -24,7 +27,7 @@ function displayItems() {
     // console.log(res);
 
     var table = new Table({
-      head: ["ITEM ID", "NAME", "DEPARTMENT", "PRICE"],
+      head: ["ITEM ID", "PRODUCT NAME", "DEPARTMENT NAME", "PRICE"],
       colWidths: [10, 50, 35, 12]
     });
 
@@ -32,50 +35,70 @@ function displayItems() {
       table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price]);
     }
     console.log(table.toString());
+    console.log("");
+    console.log("Please answer the questions below, so we can determine if the requested item is in stock on Bamazon.");
+    console.log("");
+    console.log("---------------------------------------------------------------------------------------------------------------");
+    console.log("");
+    start();
   });
-
-  start();
 }
 
 function start() {
   inquirer
-    .prompt({
+    .prompt([{
       name: "id",
       message: "What is the ID of the product that you would like to buy?",
-      type: "list",
-      
-      /*
-      // Validate input
-      validate: function (input) {
-        if (value.length < 10) {
+      type: "input",
+      // Make sure the user input is a integer less than 10
+      validate: function(value) {
+        if (isNaN(value) === false && value <= 10) {
           return true;
-          // Declare function as asynchronous, and save the done callback
-          var done = this.async();
         }
-        else {
-          return "Please choose an ID from the list.";
-        }
+        return false;
       }
-      */
-    } /*,
+    },
     {
       name: "units",
       message: "How many units of the product would you like to buy?",
       type: "input",
-    }*/
-    )
-    .then(function (action) {
-
-      // Check to see if the store has enough of the product to meet the customers request.
-      /*
-      switch (action.answers) {
-        // 
-        case true:
-
+      validate: function(value) {
+        if (isNaN(value) === false) {
+          return true;
+        }
+        return false;
       }
-      */
-     console.log("CONSOLE: INSIDE THE .THEN STATEMENT")
+    }]).then(function (answer) {
+      console.log("");
+      console.log("---------------------------------------------------------------------------------------------------------------");
+      console.log("");
+      console.log("You requested " + answer.units + " units of item id " + answer.id + ".")
+      console.log("");
+        
+      var query="SELECT * FROM products WHERE ?"
+      connection.query(query, {item_id:answer.id}, function(err, res){
+        // console.log(res);
+        console.log("Currently the stock quantity of item id " + answer.id + " is: " + res[0].stock_quantity);
+        console.log("");
+        console.log("Your total is $" + (parseInt(answer.units) * res[0].price));
+
+        if (answer.units <= res[0].stock_quantity) {
+          var updateQuery = ("UPDATE products SET stock_quantity =" + (res[0].stock_quantity - answer.units) + " WHERE item_id = " + answer.id);
+          connection.query(updateQuery, {item_id:answer.id}, function(err, res) {
+
+          //console.log("Your total is " + (parseInt(answer.units) * res[0].price));
+          console.log(""); 
+          console.log("Thanks for shopping with us!");
+          console.log("");
+          console.log("---------------------------------------------------------------------------------------------------------------");
+          });
+        }
+        else if (answer.units > res[0].stock_quantity) {
+          console.log(""); 
+          console.log("Sorry! We do not have enough in stock right now. Please check back later.");
+          console.log("");
+          console.log("---------------------------------------------------------------------------------------------------------------");
+        }
+      });
     });
 };
-
-// condition
